4a145bd2f11fdf413898a539fb0df031a67d59b5
Urs Utzinger <uutzinger@gmail.com>
2020-01-04 22:25:56 -0700

    SS 220

--- installBasics.sh
+++ installBasics.sh
@@ -1,80 +1,60 @@
-###!/bin/bash -e
+##############################################################
 #
 # Much of this camre frome here:
 # https://github.com/wpilibsuite/FRCVision-pi-gen/
 #
+##############################################################
 
 # Set date closer to actual date, otherwise ntp might not work on your network
 sudo date -s “Tue Nov 1 10:38:00 MST 2016”
 
-# Add PGPG Key
-#sudo apt-key add - < files/raspberry.gpg.key
-
 ##############################################################
-#set hostname e.g. frcvision
+#set hostname to the computer name you want e.g.  frcvision
+sudo hostnamectl set-hostname bme210_teamname
+
 
 # 1) Install packages and apps beyond standard buster installation
 ##################################################################
 
-# Basics, are already on Buster
-#
-#sudo apt-get -y install raspi-config rpi-update
-#sudo apt-get -y install netbase
-#sudo apt-get -y install raspi-copies-and-fills
-#sudo apt-get -y install ssh less fbset psmisc strace ed ncdu crda
-#sudo apt-get -y install console-setup keyboard-configuration debconf-utils
-#sudo apt-get -y install avahi-daemon
-#sudo apt-get -y install luajit
-#sudo apt-get -y install hardlink ca-certificates
-#sudo apt-get -y install fake-hwclock usbutils
-#sudo apt-get -y install dphys-swapfile
-#sudo apt-get -y install raspberrypi-sys-mods 
-#sudo apt-get -y install apt-listchanges
-#sudo apt-get -y install usb-modeswitch
-#sudo apt-get -y install libpam-chksshpwd
-#sudo apt-get -y install libmtp-runtime
-#sudo apt-get -y install htop
-#sudo apt-get -y install policykit-1
-#sudo apt-get -y install ssh-import-id
-#sudo apt-get -y install rng-tools
-#sudo apt-get -y install ethtool
-#sudo apt-get -y install cifs-utils
-#
+# Basics
 sudo apt-get -y install coreutils vim quilt qemu-user-static 
-sudo apt-get -y install debootstrap zerofree pxz zip unzip dosfstools
-sudo apt-get -y install bsdtar libcap2-bin 
-sudo apt-get -y install parted grep rsync xz-utils file git curl
-sudo apt-get -y install xxd udev xz-utils
-sudo apt-get -y install build-essential gdb cmake cmake-qt-gui unzip pkg-config 
+sudo apt-get -y install zerofree pxz zip bsdtar
+sudo apt-get -y install cmake cmake-qt-gui 
 sudo apt-get -y install daemontools daemontools-run
-sudo apt-get -y install busybox-syslogd
 sudo apt-get -y install gdbserver
 sudo apt-get -y install lua5.2
-# GTK programmers interface, takes a while
-sudo apt-get -y install libgtkmm-3.0-dev libarchive-dev libcurl4-openssl-dev intltool
-# Package maintenance
-sudo apt-get -y install devscripts debhelper 
+sudo apt-get -y install libarchive-dev libcurl4-openssl-dev intltool
+
+# Package builder tools
+#sudo apt-get -y install devscripts debhelper 
+
 # Get the video and image libraries
-sudo apt-get -y install v4l-utils
-sudo apt-get -y install libjpeg-dev libpng-dev libtiff-dev libjasper-dev
+sudo apt-get -y install libjpeg-dev libtiff-dev libtiff5-dev libjasper-dev
 sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libavresample-dev
 sudo apt-get -y install libxvidcore-dev libx264-dev
+
+# openCV likes these
+sudo apt-get -y install libfontconfig1-dev libcairo2-dev
+sudo apt-get -y install libgdk-pixbuf2.0-dev libpango1.0-dev
+sudo apt-get -y install python3-pyqt5
+
+# GTK 
+sudo apt-get -y install libgtkmm-3.0-dev
 sudo apt-get -y install libgtk2.0-dev libgtk-3-dev
 sudo apt-get -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 sudo apt-get -y install libcanberra-gtk*
 sudo apt-get -y install libhdf5-dev
+
+# QT
 sudo apt-get -y install libqtwebkit4 libqt4-test
+
 # Get numerical computation packages
 sudo apt-get -y install libopenblas-dev liblapacke-dev
 sudo apt-get -y install libatlas-base-dev libblas-dev gfortran
 sudo apt-get -y install libboost-all-dev
 sudo apt-get -y install libeigen{2,3}-dev liblapack-dev
+
+
 # Network
-#sudo apt-get -y install wpasupplicant wireless-tools 
-#sudo apt-get -y install firmware-atheros firmware-brcm80211 
-#sudo apt-get -y install firmware-libertas firmware-misc-nonfree firmware-realtek
-#sudo apt-get -y install raspberrypi-net-mods
-#sudo apt-get -y install dhcpcd5
-#sudo apt-get -y install net-tools
 sudo apt-get -y install ntp
 sudo apt-get -y install libldap2-dev
--- installSystem.sh
+++ installSystem.sh
@@ -1,5 +1,3 @@
-###!/bin/bash -e
-#
 ##############################################################
 # Urs' desired RasPi Setup
 ##############################################################
@@ -11,34 +9,42 @@ sudo apt-get dist-upgrade -y
 git clone https://github.com/StuartIanNaylor/zram-config
 cd zram-config
 sudo sh install.sh
-sudo nano /etc/ztab # modiy if necessary
+# modiy if necessary
+#sudo nano /etc/ztab
 
 # Conky
 #######
 cd ~
 sudo apt-get -y install conky-all
 wget -O /home/pi/.conkyrc https://raw.githubusercontent.com/novaspirit/rpi_conky/master/rpi3_conkyrc
-sudo nano /etc/xdg/lxsession/LXDE/autostart # add @conky at end of file
+sudo nano /etc/xdg/autostart/conky.desktop 
+# add folliwing lines to the file
+[Desktop Entry]
+Name=conky
+Type=Application
+Exec=sh /usr/bin/conky.sh
+Terminal=false
+Comment=system monitoring tool.
+Categories=Utility;
 
 # Configure Raspi interfaces
 ################
 sudo raspi-config
 # Enable Boot to Desktop, log in as user pi
 # Enable VNC, I2C, SPI, Camera
+
 # Setup video to create display even when no display is attached
 ################################################################
-#
 # Configure for VNC even when no monitor is attached to system
 sudo nano /boot/config.txt
 add:
 hdmi_force_hotplug=1
 hdmi_group=2
 hdmi_mode=85
-sudo reboot
 
 # Visual Code 
 # This editor needs more resouces than others but I still prefer and it works on RasPi
-#############
+################################################################
 wget https://packagecloud.io/headmelted/codebuilds/gpgkey
 sudo apt-key add gpgkey
 sudo -s
@@ -51,17 +57,15 @@ exit
 # Python
 #############
 cd ~
-sudo apt-get -y install python3 python3-distutils
-sudo apt-get -y install python3-dev
-sudo apt-get -y install python3-pybind11 python3-pip python3-rpi.gpio
+sudo apt-get -y install python3-pybind11
 sudo apt-get -y install python-pybind11
-sudo apt-get -y install libpython3-dev
 sudo apt-get -y install libusb-1.0-0-dev
 sudo apt-get -y install swig
+sudo apt-get -y install gfortran
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
+sudo python2 get-pip.py
 sudo pip3 install --upgrade setuptools
-sudo pip3 install numpy
 
 # Python compiler
 # picamera
@@ -71,20 +75,16 @@ sudo pip3 install cython
 sudo pip3 install "picamera[array]"
 sudo pip3 install imutils
 git clone https://github.com/jrosebr1/imutils.git
-sudo pip3 install scipy 					# lots of stuff
+sudo pip3 install scipy
 sudo pip3 install scikit-image
 sudo pip3 install dlib
-sudo pip3 install face_recognition			#
-sudo pip3 install zmq						# perhaps we will replace networktables with this one sometime
+sudo pip3 install face_recognition
+sudo pip3 install zmq
+sudo pip3 install opencv-contrib-python==4.1.0.25
 
 # I/O 
 # Allow for digitial input output and its support tools
-# Instll drivers for a few sensors and use Adafruit Blinka and PureIO as backbone
 #############
-sudo apt-get -y install python-smbus
-sudo apt-get -y install i2c-tools
-sudo pip3 install RPI.GPIO
-#
 git clone https://github.com/adafruit/Adafruit_Python_PureIO.git
 cd Adafruit_Python_PureIO
 sudo python3 setup.py install
@@ -116,3 +116,9 @@ export ANT_HOME=/usr/share/ant
 export PATH=${PATH}:${ANT_HOME}/bin
 export JAVA_HOME=/usr/lib/jvm/openjdk-11
 export PATH=${PATH}:$JAVA_HOME/bin
+
+# Camera Viewer
+sudo apt-get -y install cheese
+sudo apt-get -y install luvcview
+sudo apt-get -y install fswebcam
+
